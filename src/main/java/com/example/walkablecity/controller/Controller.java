aa92ee97e9d4b6a2881822d6a0e3106d884a2998
ArkimCity <jaeung949@gmail.com>
2021-02-03 09:57:19 +0900

    added Crawler

--- src/main/java/com/example/walkablecity/controller/Controller.java
+++ src/main/java/com/example/walkablecity/controller/Controller.java
@@ -3,11 +3,21 @@ package com.example.walkablecity.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.example.walkablecity.model.Crawler.relatedNaverCrawler;
+
 @RestController
 public class Controller {
 
     @GetMapping("gettest")
     public String m1() {
         return "재웅 짱짱맨";
+    }
+
+    @GetMapping("crawltest")
+    public ArrayList<String> m2(String search) throws IOException {
+        return relatedNaverCrawler(search);
     }
 }
