40a4afa298aefbeafcf51029a6012249f2090c29
Jaeung Kim <jaeung949@gmail.com>
2021-02-11 18:16:07 +0900

    vue init

---
+++ src/components/SeoulGeo.vue
@@ -0,0 +1,12 @@
+<script>
+
+fetch('../seoulGeo/AL_11_D162_20200917.json').then((response)=>{
+    return response.json();
+}).then((gJson)=>{
+    console.log(gJson["features"][0]);
+})
+
+export default {
+    
+}
+</script>
