67b1f03be3d7afdfae0f899b6ed3d81128005fda
Jaeung Kim <jaeung949@gmail.com>
2021-03-05 22:43:57 +0900

    practice

---
+++ goyan/src/App.js
@@ -0,0 +1,47 @@
+import api from './api.js'
+import ImageInfo from './ImageInfo.js'
+import SearchInput from './SearchInput.js'
+import SearchResult from './SearchResult.js'
+
+console.log("app is running!");
+
+export class App {
+  $target = null;
+  data = [];
+
+  constructor($target) {
+    this.$target = $target;
+
+    this.searchInput = new SearchInput({
+      $target,
+      onSearch: keyword => {
+        api.fetchCats(keyword).then(({ data }) => this.setState(data));
+      }
+    });
+
+    this.searchResult = new SearchResult({
+      $target,
+      initialData: this.data,
+      onClick: image => {
+        this.imageInfo.setState({
+          visible: true,
+          image
+        });
+      }
+    });
+
+    this.imageInfo = new ImageInfo({
+      $target,
+      data: {
+        visible: false,
+        image: null
+      }
+    });
+  }
+
+  setState(nextData) {
+    console.log(this);
+    this.data = nextData;
+    this.searchResult.setState(nextData);
+  }
+}
