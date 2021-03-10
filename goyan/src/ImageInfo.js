3a6605edd5e60cf2940ce57c32870b1e191f493f
Jaeung Kim <jaeung949@gmail.com>
2021-03-10 23:07:50 +0900

    update

--- goyan/src/ImageInfo.js
+++ goyan/src/ImageInfo.js
@@ -1,47 +1,53 @@
 export default class ImageInfo {
-    $imageInfo = null;
-    data = null;
-  
-    constructor({ $target, data }) {
-      const $imageInfo = document.createElement("div");
-      $imageInfo.className = "ImageInfo";
-      this.$imageInfo = $imageInfo;
-      $target.appendChild($imageInfo);
-  
-      this.data = data;
-  
-      this.render();
-    }
-  
-    setState(nextData) {
-      this.data = nextData;
-      this.render();
-    }
-  
-    render() {
-      if (this.data.visible) {
-        const { name, url, temperament, origin } = this.data.image;
-  
-        this.$imageInfo.innerHTML = `
-          <div class="content-wrapper">
-            <div class="title">
-              <span>${name}</span>
-              <div class="close">x</div>
-            </div>
-            <img src="${url}" alt="${name}"/>        
-            <div class="description">
-              <div>성격: ${temperament}</div>
-              <div>태생: ${origin}</div>
-            </div>
-          </div>`;
-        this.$imageInfo.style.display = "block";
-        
-        this.$imageInfo.querySelector('.close').addEventListener('click', ()=>{
-          this.$imageInfo.style.display = "none";
-        })
-      } else {
+  $imageInfo = null;
+  data = null;
+
+  constructor({ $target, data }) {
+    const $imageInfo = document.createElement("div");
+    $imageInfo.className = "ImageInfo";
+    this.$imageInfo = $imageInfo;
+    $target.appendChild($imageInfo);
+
+    this.data = data;
+
+    this.render();
+  }
+
+  setState(nextData) {
+    this.data = nextData;
+    this.render();
+  }
+
+  render() {
+    if (this.data.visible) {
+      const { name, url, temperament, origin } = this.data.image;
+
+      this.$imageInfo.innerHTML = `
+        <div class="content-wrapper">
+          <div class="title">
+            <span>${name}</span>
+            <div class="close">x</div>
+          </div>
+          <img src="${url}" alt="${name}"/>        
+          <div class="description">
+            <div>성격: ${temperament}</div>
+            <div>태생: ${origin}</div>
+          </div>
+        </div>`;
+      this.$imageInfo.style.display = "block";
+      
+      this.$imageInfo.querySelector('.close').addEventListener('click', ()=>{
         this.$imageInfo.style.display = "none";
-      }
+      })
+
+      document.addEventListener("keyup", e => {
+        if (e.keyCode === 27 && this.$imageInfo.style.display == "block") {
+          this.$imageInfo.style.display = "none";
+        }
+      });
+    } else {
+      this.$imageInfo.style.display = "none";
     }
   }
+}
   
